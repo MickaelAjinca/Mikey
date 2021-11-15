779486ed6cd848050e497b2ea8c3d56df0cf2aa5
MickaelAjinca <Mickajin@gmail.com>
2021-11-15 16:59:06 +0100

    ajout titre h2

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-
-
-?>
-
 <!doctype html>
 <html lang="fr">
 
@@ -21,23 +16,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="style.css">
+
+
     <title>Mikey</title>
     <style>
+
     body {
-        font-family: 'Lato', sans-serif;
-        font-family: 'Merriweather Sans', sans-serif;
-    }
+    font-family: 'Lato', sans-serif;
+    font-family: 'Merriweather Sans', sans-serif;
+}
 
-    h1 {
-        font-family: 'Merriweather Sans', sans-serif;
-    }
+h1 {
+    font-family: 'Merriweather Sans', sans-serif;
+}
 
-    #img {
-        height: 20%;
-        width: 20%;
-        
-    }
-    </style>
+#img {
+    height: 20%;
+    width: 20%;
+
+}
+</style>
+
 </head>
 
 <body>
@@ -57,80 +57,101 @@
     </ul>
 
     <h1 class="text-center display-4 my-4">Mikey</h1>
- <div class="container">
+    <h2>Bonsoir PARISSSSSS!!!!!!!!!!!!!!!!!!!!!!</h2>
+    <div class="container">
 
- <form class="row g-3">
- <div class="col-auto">
-    <label for="pseudo" class="form-label">Pseudo</label>
-    <input type="text" class="form-control" id="pseudo">
-  </div>
-  <div class="col-auto">
-    <label for="mot_de_passe" class="form-label">Mot de passe</label>
-    <input type="text" class="form-control" id="mot_de_passe">
-  </div>
-  <div class="col-auto">
-    <label for="avis" class="form-label">Votez pour le pilote du jour</label>
-    <input type="text" class="form-control" id="avis">
-  </div>
-  <div class="col-auto">
-    <label for="np" class="form-label">Pilote du jour</label>
-    <input type="text" class="form-control" id="np" placeholder="Nom du pilote du jour">
-  </div>
-  <div class="col-auto">
-    <button type="submit" class="btn btn-primary mb-3">Envoyer l'avis</button>
-  </div>
-</form>
+        <form class="row g-3">
+            <div class="col-auto">
+                <label for="pseudo" class="form-label">Pseudo</label>
+                <input type="text" class="form-control" id="pseudo">
+            </div>
+            <div class="col-auto">
+                <label for="mot_de_passe" class="form-label">Mot de passe</label>
+                <input type="text" class="form-control" id="mot_de_passe">
+            </div>
+            <div class="col-auto">
+                <label for="avis" class="form-label">Votez pour le pilote du jour</label>
+                <input type="text" class="form-control" id="avis">
+            </div>
+            <div class="col-auto">
+                <label for="np" class="form-label">Pilote du jour</label>
+                <input type="text" class="form-control" id="np" placeholder="Nom du pilote du jour">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">Envoyer l'avis</button>
+            </div>
+        </form>
 
 
-<h1 class="text-center">Merci d'avoir voté</h1>
+        <h1 class="text-center display-4 my-4">Merci d'avoir voté</h1>
 
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat voluptatum quod esse inventore impedit pariatur consequatur aperiam cupiditate est repudiandae explicabo sit debitis nostrum hic ipsum quidem vel itaque id dignissimos, labore doloribus accusantium ducimus. Voluptatem sint accusantium dolor, vel quas doloremque praesentium blanditiis suscipit!
-    </p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat voluptatum quod esse inventore impedit pariatur consequatur aperiam cupiditate est repudiandae explicabo sit debitis nostrum hic ipsum quidem vel itaque id dignissimos, labore doloribus accusantium ducimus. Voluptatem sint accusantium dolor, vel quas doloremque praesentium blanditiis suscipit!
-    </p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat voluptatum quod esse inventore impedit pariatur consequatur aperiam cupiditate est repudiandae explicabo sit debitis nostrum hic ipsum quidem vel itaque id dignissimos, labore doloribus accusantium ducimus. Voluptatem sint accusantium dolor, vel quas doloremque praesentium blanditiis suscipit!
-    </p>
+        <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat voluptatum quod esse inventore impedit
+            pariatur consequatur aperiam cupiditate est repudiandae explicabo sit debitis nostrum hic ipsum quidem vel
+            itaque id dignissimos, labore doloribus accusantium ducimus. Voluptatem sint accusantium dolor, vel quas
+            doloremque praesentium blanditiis suscipit!
+        </p>
+        <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat voluptatum quod esse inventore impedit
+            pariatur consequatur aperiam cupiditate est repudiandae explicabo sit debitis nostrum hic ipsum quidem vel
+            itaque id dignissimos, labore doloribus accusantium ducimus. Voluptatem sint accusantium dolor, vel quas
+            doloremque praesentium blanditiis suscipit!
+        </p>
+        <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat voluptatum quod esse inventore impedit
+            pariatur consequatur aperiam cupiditate est repudiandae explicabo sit debitis nostrum hic ipsum quidem vel
+            itaque id dignissimos, labore doloribus accusantium ducimus. Voluptatem sint accusantium dolor, vel quas
+            doloremque praesentium blanditiis suscipit!
+        </p>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="image/Silverston.jpg" class="d-block" id="img" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/hamilton.jpg" class="d-block " id="img" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/lecler.jpg" class="d-block" id="img" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">course précédente</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">course suivante</span>
+            </button>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="image/Silverston.jpg" class="d-block" id="img" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="image/hamilton.jpg" class="d-block " id="img" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="image/lecler.jpg" class="d-block" id="img" alt="...">
-            </div>
+
+        <h4 class="text-center display-4 my-4">A propos de Moi</h4>
+
+        <div class="d-flex align-item-center">
+            <img src="image/LH.jpg" alt="" id="img">
+            <P class="text-center border border-primary">Je suis Mickaël Ajinça, en formule 1 je supporte Lewis Hamilton
+                qui est le meilleur pilote de l'histoire de la F1, il est le plus grand champion de la catégorie avec la
+                meilleure équipe de ses dernières années Mercedes F1 Team.</P>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">course précédente</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">course suivante</span>
-        </button>
+
+        <section>
+            <div class="d-flex justify-content-end">
+                <img src="image/bayern-munich.jpg" alt="" id="img">
+                <P class="text-center border border-primary">Pour le football, je supporte le Bayern Munich depuis petit
+                    et mes deux joeurs préferés Coman et Robben.</P>
+            </div>
+        </section>
     </div>
 
- </div>
-<footer>
-  <h4 class="text-center">A propos de Moi</h4>
-  
-  <img src="image/LH.jpg" alt="" id="img" class="d-block">
-  <P class="text-center">Je suis Mickaël Ajinça, en formuleje supporte Lewis Hamilton qui est le meilleur pilote de l'histoire de la F1, il est le plus grand champion de la catégorie avec la meilleure équipe de ses dernières années Mercedes F1 Team.</P>
 
-</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
