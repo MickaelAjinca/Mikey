cdf4a24c8e20f367bb59cb23cfe85189fd23936e
MickaelAjinca <Mickajin@gmail.com>
2021-11-15 12:24:40 +0100

    initialisation du projet + première version

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@1,300&display=swap"
+        rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Mikey</title>
+    <style>
+    body {
+        font-family: 'Lato', sans-serif;
+        font-family: 'Merriweather Sans', sans-serif;
+    }
+
+    h1 {
+        font-family: 'Merriweather Sans', sans-serif;
+    }
+    </style>
+</head>
+
+<body>
+    <h1 class="text-center display-4 my-4">Mikey</h1>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
